bae9719b701a6b54c5fc378eb0da22328ef243b2
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-03-26 16:40:23 +0100

    Dropped Python 3.5 support + Added Python 3.7 support

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     setup_requires=[
