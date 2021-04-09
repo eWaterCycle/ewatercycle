3f9b53f93d4ff3db3c2ba2d97279420d08b1a751
Peter Kalverla <peter.kalverla@gmx.com>
2021-04-09 11:39:22 +0200

    working version of wflow model implementation

--- ewatercycle/models/wflow.py
+++ ewatercycle/models/wflow.py
@@ -1,8 +1,10 @@
+import shutil
 from pathlib import Path
 from typing import Any, Iterable, Optional, Tuple
 
 import numpy as np
 import xarray as xr
+from cftime import num2date
 from grpc4bmi.bmi_client_docker import BmiClientDocker
 from grpc4bmi.bmi_client_singularity import BmiClientSingularity
 
@@ -12,7 +14,7 @@ from ewatercycle.models.abstract import AbstractModel
 
 # mock CFG until the CFG PR is merged.
 CFG = {
-    "container_engine": "singularity",
+    "container_engine": "docker",
     "singularity_images.wflow": "ewatercycle-wflow-grpc4bmi.sif",
     "docker_images.wflow": "ewatercycle/wflow-grpc4bmi:latest",
 }
@@ -25,106 +27,98 @@ class Wflow(AbstractModel):
         bmi (Bmi): GRPC4BMI Basic Modeling Interface object
     """
     def setup(self,
-              work_dir: Path,
-              inifile: Path,
-              parameterset: Optional[Path] = None,
-              forcing_data: Optional[Path] = None):
-        """Performs model setup.
-
-        1. Creates config file and config directory
-        2. Start bmi container and store as self.bmi
+              work_dir: str,
+              inifile: str,
+              parameterset: Optional[str] = None,
+              forcing_data: Optional[str] = None):
+        """Start the model inside a container and return a valid config file.
 
         Args:
 
             - work_dir: a writable folder that will be used as the main working
-            directory for running the model
+            directory for running the model. This directory should either be
+            pre-populated with valid parametersets and forcing, or the user
+            should specify the additional arguments `parameterset` and
+            `forcing_data`.
 
             - inifile: path to the configuration file, typically somethig like
-            `work_dir/wflow_sbm.ini`
+            `wflow_sbm.ini`.
 
             - parameterset (optional): path to a valid wflow model spec. If
-            given, the parameterset will be copied to `work_dir`
+            given, the parameterset will be copied to `work_dir`. This is useful
+            if you can't or don't want to write to this folder.
 
             - forcing_data: path to meteorological forcing data (.nc) file. If
             given, will be copied to work_dir. If not given, it should already
             be there.
 
         Returns:
-            Path to config file and path to config directory
+            Path to config file
         """
-        # work_dir = ....
-        config_file = work_dir / inifile.name
+        config_file = Path(work_dir) / Path(inifile).name
 
-        # For Wflow we have to copy the input (which is unwritable)
-        # to a working directory (what is writable)
-        shutil.copytree(src=parameterset, dst=work_dir)
-        shutil.copy(src=forcing_file, dst=work_dir)
+        if parameterset is not None:
+            shutil.copytree(src=parameterset, dst=work_dir)
 
-        # Modify the path to the forcing data in the config file
-        cfg = ConfigParser()
-        cfg.optionxform = lambda x: x
-        cfg.read(inifile)
-        cfg.set("framework", "netcdfinput", forcing_file.name)
-        with open(config_file, "w") as filename:
-            cfg.write(filename)
+        if forcing_data is not None:
+            shutil.copy(src=forcing_file, dst=work_dir)
+
+            cfg = ConfigParser()
+            cfg.optionxform = lambda x: x
+            cfg.read(inifile)
+            cfg.set("framework", "netcdfinput", Path(forcing_file).name)
+
+            with open(config_file, "w") as filename:
+                cfg.write(filename)
 
         # Start the container
-        if CFG.container_engine.lower() == "docker":
+        if CFG["container_engine"] == "docker":
             self.bmi = BmiClientDocker(
                 image=CFG["docker_images.wflow"],
                 image_port=55555,
                 work_dir=work_dir,
+                timeout=10,
             )
-        elif CFG.container_engine.lower() == "singularity":
+        elif CFG["container_engine"] == "singularity":
+            image = CFG["singularity_images.wflow"]
+            assert Path(
+                image).exists(), f"No singularity image found at {image}"
             self.bmi = BmiClientSingularity(
-                image=CFG["singularity_images.wflow"],
+                image=image,
                 work_dir=work_dir,
+                timeout=10,
             )
         else:
             raise ValueError(
                 f"Unknown container technology in CFG: {CFG['container_engine']}"
             )
 
-        return config_file
+        print(f"Started Wflow container with working directory {work_dir}")
+        return config_file.name
 
     def get_value_as_xarray(self, name: str) -> xr.DataArray:
-        # TODO this is just copied from the comparison notebook.
-        # TODO update this so it can work as standalone code
-
-        # Let's analyse runoff
-        variable = "RiverRunoff"
-
+        """Return the value as xarray object."""
         # Get time information
-        time_units = model.get_time_units()
-        now = num2date(model.get_current_time(), time_units)
-
-        # Get data (and convert to something that's nice in xarray)
-        grid = model.get_var_grid(variable)
-
-        # Get the model's internal configuration
-        lat = model.get_grid_x(grid)
-        lon = model.get_grid_y(grid)
-        shape = model.get_grid_shape(grid)
+        time_units = self.bmi.get_time_units()
+        grid = self.bmi.get_var_grid(name)
+        shape = self.bmi.get_grid_shape(grid)
 
         # Extract the data and store it in an xarray DataArray
-        data = np.reshape(model.get_value(variable), shape)
         da = xr.DataArray(
-            data,
+            data=np.reshape(self.bmi.get_value(name), shape),
             coords={
-                "longitude": lon,
-                "latitude": lat,
-                "time": now
+                "longitude": self.bmi.get_grid_y(grid),
+                "latitude": self.bmi.get_grid_x(grid),
+                "time": num2date(self.bmi.get_current_time(), time_units)
             },
             dims=["latitude", "longitude"],
-            name=variable,
-            attrs={"units": model.get_var_units(variable)},
+            name=name,
+            attrs={"units": self.bmi.get_var_units(name)},
         )
 
-        da.data = np.reshape(model.get_value(variable), shape)
-        da.time.values = num2date(model.get_current_time(), time_units)
         return da.where(da != -999)
 
     @property
     def parameters(self):
         """List the variable names that are available for this model."""
-        return self.model.get_output_var_names()
+        return self.bmi.get_output_var_names()
