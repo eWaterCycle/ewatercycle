efedc16d70d5e9326ebafb7efed12b51d82b17d2
Stef Smeets <stefsmeets@users.noreply.github.com>
2021-04-13 12:08:10 +0000

    Update ewatercycle/config/__init__.py
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- ewatercycle/config/__init__.py
+++ ewatercycle/config/__init__.py
@@ -3,6 +3,24 @@
 .. data:: CFG
 
     eWaterCycle configuration object.
+    
+    The configuration is loaded from 
+    1. ~/.ewatercycle/ewatercycle.yaml
+    2. /etc/ewatercycle/ewatercycle.yaml
+    3. Fall back to empty configuration
+    
+    The `ewatercycle.yaml` is formatted in YAML and could for example look like
+    
+    .. code-block:: yaml
+    
+      esmvaltool_config: ~/.esmvaltool/config-user.yml
+      grdc_location: /data/grdc
+      container_engine: singularity
+      singularity_dir: /data/singularity-images
+      output_dir: /scratch
+      # Created with cd  /data/singularity-images && singularity pull docker://ewatercycle/wflow-grpc4bmi:2020.1.1
+      wflow.singularity_images: wflow-grpc4bmi_2020.1.1.sif
+      wflow.docker_images: ewatercycle/wflow-grpc4bmi:2020.1.1
 """
 
 from ._config_object import CFG, Config
