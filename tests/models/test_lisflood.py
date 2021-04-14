23e86d3920237c88eb99401b54d2844662e7bbff
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-04-14 16:58:56 +0200

    Fix tests by mocking BmiClientSingularity

--- tests/models/test_lisflood.py
+++ tests/models/test_lisflood.py
@@ -1,10 +1,13 @@
 import shutil
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 import xarray
 
 from esmvalcore.experimental.recipe_output import DataFile
+from grpc4bmi.bmi_client_singularity import BmiClientSingularity
+
 from ewatercycle.forcing.forcing_data import ForcingData
 from ewatercycle.parametersetdb.datafiles import SubversionCopier
 from ewatercycle.models.lisflood import CFG, Lisflood, LisfloodParameterSet
@@ -15,7 +18,7 @@ def mocked_config(tmp_path):
     CFG['scratch_dir'] = tmp_path
     CFG['container_engine'] = 'singularity'
     # TODO for reproducibility use versioned label instead of latest
-    CFG['lisflood']['singularity_image'] = 'docker://ewatercycle/lisflood-grpc4bmi:latest'
+    CFG['lisflood.singularity_image'] = 'docker://ewatercycle/lisflood-grpc4bmi:latest'
 
 
 @pytest.fixture
@@ -44,9 +47,9 @@ class TestLFlatlonUseCase:
             mask_dir / 'model_mask',
         )
         return LisfloodParameterSet(
-            root=str(root),
-            mask=str(mask_dir),
-            config_template=str(root / 'settings_lat_lon-Run.xml'),
+            root=root,
+            mask=mask_dir / 'model_mask',
+            config_template=root / 'settings_lat_lon-Run.xml',
         )
 
     @pytest.fixture
@@ -80,8 +83,17 @@ class TestLFlatlonUseCase:
         return ForcingData(recipe_output)
 
     # TODO stuck during BmiClientSingularity creation
-    # def test_setup(self, mocked_config, model, forcing, parameterset):
-    #     config_file, config_dir = model.setup(forcing, parameterset)
-    #
-    #     assert '/scratch/shared/ewatercycle' in config_dir
-    #     assert config_file == 'lisflood_setting.xml'
+    def test_setup(self, mocked_config, model, forcing, parameterset, tmp_path):
+        with patch.object(BmiClientSingularity, '__init__', return_value=None) as mocked_constructor, patch(
+              'time.strftime', return_value='42'):
+            config_file, config_dir = model.setup(forcing, parameterset)
+
+            mocked_constructor.assert_called_once_with(
+                image='docker://ewatercycle/lisflood-grpc4bmi:latest',
+                input_dirs=[
+                    f'{tmp_path}/input',
+                    f'{tmp_path}/mask',
+                    f'{tmp_path}/forcing'],
+                work_dir=f'{tmp_path}/lisflood_42')
+            assert 'lisflood_42' in str(config_dir)
+            assert config_file.name == 'lisflood_setting_1986_2018.xml'
