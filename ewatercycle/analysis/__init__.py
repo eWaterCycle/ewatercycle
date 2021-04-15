f03acee9e1bf953ee257f44a5df26afaf1807b9e
Stef Smeets <s.smeets@esciencecenter.nl>
2021-04-15 16:56:48 +0200

    Add function to plot hydrograph

---
+++ ewatercycle/analysis/__init__.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import os
+from typing import Union
+from hydrostats import metrics
+import matplotlib.pyplot as plt
+
+
+def metrics_to_string(row: pd.Series) -> str:
+    """Takes a row with metrics and formats it as a label string."""
+    return (
+        f'{row.name}\n'
+        f'NSE={row.nse:.2f}; '
+        f'KGE_2009={row.kge_2009:.2f}; '
+        f'SA={row.sa:.2f}; '
+        f'ME={row.me:.2f}; '
+    )
+
+
+def hydrograph(
+        discharge: pd.DataFrame,
+        *,
+        reference: str,
+        precipitation: pd.DataFrame = None,
+        fname: Union[os.PathLike, str] = None,
+        dpi : int = None,
+        title : str = 'Hydrograph',
+        ylabel: str = 'Streamflow (m$^3$ s$^{-1}$)',
+    ):
+    """Plot a hydrograph.
+
+    Parameters
+    ----------
+    discharge : pd.DataFrame
+        Dataframe containing time series of discharge data to be plotted.
+    reference : str
+        Name of the reference data, must correspond to a column in the discharge
+        dataframe. Metrics are calculated between the reference column and each
+        of the other columns.
+    precipitation : pd.DataFrame, optional
+        Optional dataframe containing time series of precipitation data to be
+        plotted from the top of the hydrograph.
+    fname : str, Path
+        If specified, a copy of the plot will be saved to this path.
+    dpi : int
+        DPI for the plot.
+    title : str
+        Title of the hydrograph.
+    """
+    discharge_cols = discharge.columns.drop('reference')
+    y_obs = discharge[reference]
+    y_sim = discharge[discharge_cols]
+
+    def calc_metric(metric) -> float:
+        return y_sim.apply(metric, observed_array=y_obs)
+
+    metrs = pd.DataFrame({
+        'nse': calc_metric(metrics.nse),
+        'kge_2009': calc_metric(metrics.kge_2009),
+        'sa': calc_metric(metrics.sa),
+        'me': calc_metric(metrics.me),
+    })
+
+    fig, ax = plt.subplots(dpi=dpi)
+
+    y_obs.plot(ax=ax)
+    y_sim.plot(ax=ax)
+
+    handles, labels = ax.get_legend_handles_labels()
+
+    # Generate labels that include the metrics for the discharge data
+    new_labels = []
+    for label in labels:
+        if label != reference:
+            label = metrics_to_string(metrs.T[label])
+        new_labels.append(label)
+
+    # Put the legend outside the plot, at the bottom
+    ax.legend(handles, new_labels, bbox_to_anchor=(0.5, -0.15), loc='upper center')
+
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+
+    if precipitation is not None:
+        raise NotImplementedError
+
+    if fname is not None:
+        raise NotImplementedError
