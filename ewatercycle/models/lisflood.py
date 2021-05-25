20da3a9b123445bf22a4b32f74715869f82d37dd
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-05-25 17:03:27 +0200

    fix the bug

--- ewatercycle/models/lisflood.py
+++ ewatercycle/models/lisflood.py
@@ -97,16 +97,13 @@ class Lisflood(AbstractModel):
             self.singularity_image = CFG['singularity_dir'] / images[self.version]
 
     def _get_textvar_value(self, name: str):
-        name_found = None
         for textvar in self.cfg.config.iter("textvar"):
             textvar_name = textvar.attrib["name"]
             if name == textvar_name:
-                name_found == name
                 return textvar.get('value')
-        if name_found is None:
-            raise KeyError(
-                f'Name {name} not found in the config file.'
-            )
+        raise KeyError(
+            f'Name {name} not found in the config file.'
+        )
 
 
     # unable to subclass with more specialized arguments so ignore type
