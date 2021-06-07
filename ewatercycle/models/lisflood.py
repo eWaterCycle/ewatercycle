f4e11170783986c4bbc03c37d40d52776d1dcac9
SarahAlidoost <55081872+SarahAlidoost@users.noreply.github.com>
2021-06-07 13:42:35 +0200

    fix indent
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- ewatercycle/models/lisflood.py
+++ ewatercycle/models/lisflood.py
@@ -47,9 +47,9 @@ class Lisflood(AbstractModel):
     """eWaterCycle implementation of Lisflood hydrological model.
 
     Args:
-    version: pick a version for which an ewatercycle grpc4bmi docker image is available.
-    parameter_set: LISFLOOD input files. Any included forcing data will be ignored.
-    forcing: a LisfloodForcing object.
+      version: pick a version for which an ewatercycle grpc4bmi docker image is available.
+      parameter_set: LISFLOOD input files. Any included forcing data will be ignored.
+      forcing: a LisfloodForcing object.
 
     Attributes:
         bmi (Bmi): Basic Modeling Interface object
