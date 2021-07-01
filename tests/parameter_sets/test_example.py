ce6aea617729f6e2e5e862f9567153d740f81f70
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-07-01 11:38:34 +0200

    Flatten test hierarchy

--- tests/parameter_sets/test_example.py
+++ tests/parameter_sets/test_example.py
@@ -15,55 +15,58 @@ def setup_config(tmp_path):
     CFG.reload()
 
 
-class TestGoldenPath:
-    @pytest.fixture
-    def example(self, setup_config):
-        return ExampleParameterSet(
-            name='firstexample',
-            config_url='https://github.com/mymodelorg/mymodelrepo/raw/master/mymodelexample/config.ini',
-            datafiles_url='https://github.com/mymodelorg/mymodelrepo/trunk/mymodelexample',
-            directory='mymodelexample',
-            config='mymodelexample/config.ini',
-            supported_model_versions={'0.4.2'},
-        )
+@pytest.fixture
+def example(setup_config):
+    return ExampleParameterSet(
+        name='firstexample',
+        config_url='https://github.com/mymodelorg/mymodelrepo/raw/master/mymodelexample/config.ini',
+        datafiles_url='https://github.com/mymodelorg/mymodelrepo/trunk/mymodelexample',
+        directory='mymodelexample',
+        config='mymodelexample/config.ini',
+        supported_model_versions={'0.4.2'},
+    )
 
-    def test_to_config(self, example):
-        example.to_config()
 
-        assert 'firstexample' in CFG['parameter_sets']
-        expected = dict(
-            doi='N/A',
-            target_model='generic',
-            directory='mymodelexample',
-            config='mymodelexample/config.ini',
-            supported_model_versions={'0.4.2'},
-        )
-        assert CFG['parameter_sets']['firstexample'] == expected
+def test_to_config(example):
+    example.to_config()
 
-    @patch('urllib.request.urlopen')
-    @patch('subprocess.check_call')
-    def test_download(self, mock_check_call, mock_urlopen, example, tmp_path):
-        ps_dir = tmp_path / 'mymodelexample'
-        r = Mock()
-        r.read.return_value = b'somecontent'
-        mock_urlopen.return_value = r
-        mock_check_call.side_effect = lambda _: ps_dir.mkdir()
+    assert 'firstexample' in CFG['parameter_sets']
+    expected = dict(
+        doi='N/A',
+        target_model='generic',
+        directory='mymodelexample',
+        config='mymodelexample/config.ini',
+        supported_model_versions={'0.4.2'},
+    )
+    assert CFG['parameter_sets']['firstexample'] == expected
 
+
+@patch('urllib.request.urlopen')
+@patch('subprocess.check_call')
+def test_download(mock_check_call, mock_urlopen, example, tmp_path):
+    ps_dir = tmp_path / 'mymodelexample'
+    r = Mock()
+    r.read.return_value = b'somecontent'
+    mock_urlopen.return_value = r
+    mock_check_call.side_effect = lambda _: ps_dir.mkdir()
+
+    example.download()
+
+    mock_urlopen.assert_called_once_with('https://github.com/mymodelorg/mymodelrepo/raw/master/mymodelexample/config.ini')
+    mock_check_call.assert_called_once_with([
+        'svn', 'export',
+        'https://github.com/mymodelorg/mymodelrepo/trunk/mymodelexample',
+        ps_dir
+    ])
+    assert (ps_dir / 'config.ini').read_text() == 'somecontent'
+
+
+def test_download_already_exists(example, tmp_path):
+    ps_dir = tmp_path / 'mymodelexample'
+    ps_dir.mkdir()
+
+    with pytest.raises(ValueError) as excinfo:
         example.download()
 
-        mock_urlopen.assert_called_once_with('https://github.com/mymodelorg/mymodelrepo/raw/master/mymodelexample/config.ini')
-        mock_check_call.assert_called_once_with([
-            'svn', 'export',
-            'https://github.com/mymodelorg/mymodelrepo/trunk/mymodelexample',
-            ps_dir
-        ])
-        assert (ps_dir / 'config.ini').read_text() == 'somecontent'
+    assert str(excinfo.value) == 'Directory already exists, will not overwrite'
 
-    def test_download_already_exists(self, example, tmp_path):
-        ps_dir = tmp_path / 'mymodelexample'
-        ps_dir.mkdir()
-
-        with pytest.raises(ValueError) as excinfo:
-            example.download()
-
-        assert str(excinfo.value) == 'Directory already exists, will not overwrite'
