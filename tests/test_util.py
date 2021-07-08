e7f4410299c959c4a8898a294627f86ea144a901
Peter Kalverla <peter.kalverla@gmx.com>
2021-07-08 14:37:27 +0200

    Fix test for find_closest_point

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,28 +7,31 @@ from numpy.testing import assert_array_almost_equal
 
 
 def test_get_time_with_utc():
-    dt = get_time('1989-01-02T00:00:00Z')
+    dt = get_time("1989-01-02T00:00:00Z")
     assert dt == datetime(1989, 1, 2, tzinfo=timezone.utc)
 
 
 def test_get_time_with_cet():
     with pytest.raises(ValueError) as excinfo:
-        get_time('1989-01-02T00:00:00+01:00')
+        get_time("1989-01-02T00:00:00+01:00")
 
-    assert 'not in UTC' in str(excinfo.value)
+    assert "not in UTC" in str(excinfo.value)
 
 
 def test_get_time_without_tz():
     with pytest.raises(ValueError) as excinfo:
-        get_time('1989-01-02T00:00:00')
+        get_time("1989-01-02T00:00:00")
 
-    assert 'not in UTC' in str(excinfo.value)
+    assert "not in UTC" in str(excinfo.value)
 
 
 def test_find_closest_point():
-    expected_distances, expected_index = (
-        [[118.77417243, 111.22983323],[122.9552163 , 115.67902656]],
-        1)
-    actual_distances, actual_index = find_closest_point([-99.83, -99.32], [42.25, 42.21], -99.32, 43.25)
-    assert_array_almost_equal(actual_distances, expected_distances)
-    assert actual_index == expected_index
+    distance, idx_lon, idx_lat = find_closest_point(
+        grid_longitudes=[-99.83, -99.32],
+        grid_latitudes=[42.25, 42.21],
+        point_longitude=-99.32,
+        point_latitude=43.25,
+    )
+    assert idx_lon == 1
+    assert idx_lat == 0
+    assert_array_almost_equal(distance, [111.22983323])
