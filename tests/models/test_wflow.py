4869566ae4bb7d06715e2d7f3fb600679ba328e3
Peter Kalverla <peter.kalverla@gmx.com>
2021-07-08 11:14:47 +0200

    format and reorder tests for wflow

--- tests/models/test_wflow.py
+++ tests/models/test_wflow.py
@@ -11,6 +11,7 @@ from ewatercycle import CFG
 from ewatercycle.models import Wflow
 from ewatercycle.parameter_sets import ParameterSet
 
+
 class MockedBmi(Bmi):
     """Pretend to be a real BMI model."""
 
@@ -39,11 +40,11 @@ class MockedBmi(Bmi):
 
 @pytest.fixture
 def mocked_config(tmp_path):
-    CFG['output_dir'] = tmp_path
-    CFG['container_engine'] = 'singularity'
-    CFG['singularity_dir'] = tmp_path
-    CFG['parameterset_dir'] = tmp_path / 'psr'
-    CFG['parameter_sets'] = {}
+    CFG["output_dir"] = tmp_path
+    CFG["container_engine"] = "singularity"
+    CFG["singularity_dir"] = tmp_path
+    CFG["parameterset_dir"] = tmp_path / "psr"
+    CFG["parameter_sets"] = {}
 
 
 @pytest.fixture
@@ -75,6 +76,32 @@ def initialized_model(model):
     return model
 
 
+def test_setup(model):
+    with patch.object(
+        BmiClientSingularity, "__init__", return_value=None
+    ), patch("datetime.datetime") as mocked_datetime:
+        mocked_datetime.now.return_value = datetime(2021, 1, 2, 3, 4, 5)
+
+        cfg_file, cfg_dir = model.setup()
+
+    expected_cfg_dir = CFG["output_dir"] / "wflow_20210102_030405"
+    assert cfg_dir == str(expected_cfg_dir)
+    assert cfg_file == str(expected_cfg_dir / "wflow_ewatercycle.ini")
+
+
+def test_setup_with_custom_cfg_dir(model, tmp_path):
+    my_cfg_dir = str(tmp_path / "mycfgdir")
+    with patch.object(
+        BmiClientSingularity, "__init__", return_value=None
+    ), patch("datetime.datetime") as mocked_datetime:
+        mocked_datetime.now.return_value = datetime(2021, 1, 2, 3, 4, 5)
+
+        cfg_file, cfg_dir = model.setup(cfg_dir=my_cfg_dir)
+
+    assert cfg_dir == my_cfg_dir
+    assert cfg_file == str(Path(my_cfg_dir) / "wflow_ewatercycle.ini")
+
+
 def test_get_value_as_coords(initialized_model):
     model = initialized_model
 
@@ -83,25 +110,3 @@ def test_get_value_as_coords(initialized_model):
     assert result == expected
     assert model.bmi.indices == [2]
     return Wflow(version="2020.1.1", parameter_set=parameter_set)
-
-
-def test_setup(model):
-    with patch.object(BmiClientSingularity, '__init__', return_value=None), patch('datetime.datetime') as mocked_datetime:
-        mocked_datetime.now.return_value = datetime(2021, 1, 2, 3, 4, 5)
-
-        cfg_file, cfg_dir = model.setup()
-
-    expected_cfg_dir = CFG['output_dir'] / 'wflow_20210102_030405'
-    assert cfg_dir == str(expected_cfg_dir)
-    assert cfg_file == str(expected_cfg_dir / 'wflow_ewatercycle.ini')
-
-
-def test_setup_with_custom_cfg_dir(model, tmp_path):
-    my_cfg_dir = str(tmp_path / 'mycfgdir')
-    with patch.object(BmiClientSingularity, '__init__', return_value=None), patch('datetime.datetime') as mocked_datetime:
-        mocked_datetime.now.return_value = datetime(2021, 1, 2, 3, 4, 5)
-
-        cfg_file, cfg_dir = model.setup(cfg_dir=my_cfg_dir)
-
-    assert cfg_dir == my_cfg_dir
-    assert cfg_file == str(Path(my_cfg_dir) / 'wflow_ewatercycle.ini')
