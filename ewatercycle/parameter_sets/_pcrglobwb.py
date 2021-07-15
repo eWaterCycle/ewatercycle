0fc713138fd533fa8bb60af91f450755e02a9f73
Peter Kalverla <peter.kalverla@gmx.com>
2021-07-15 10:09:40 +0200

    Polish PCRGlob notebook (#167)
    * Add logo to repo
    
    * First pass of polishing pcrglob notebook
    
    * Minor aestethic changes
    
    * Revert to using SIF image for PCRGlobWB
    
    * pass tests with updated name of pcrglob example parameterset
    
    * Only fetch singularity dir when requested
    
    * Implement Sarah's suggestions and more polishing
    
    * Update system setup instructions
    
    * Make forcing optional

--- ewatercycle/parameter_sets/_pcrglobwb.py
+++ ewatercycle/parameter_sets/_pcrglobwb.py
@@ -7,10 +7,10 @@ def example_parameter_sets() -> Iterable[ExampleParameterSet]:
     return [
         ExampleParameterSet(
             # Relative to CFG['parameterset_dir']
-            directory="pcrglobwb_example_case",
-            name="pcrglobwb_example_case",
+            directory="pcrglobwb_rhinemeuse_30min",
+            name="pcrglobwb_rhinemeuse_30min",
             # Relative to CFG['parameterset_dir']
-            config="pcrglobwb_example_case/setup_natural_test.ini",
+            config="pcrglobwb_rhinemeuse_30min/setup_natural_test.ini",
             datafiles_url="https://github.com/UU-Hydro/PCR-GLOBWB_input_example/trunk/RhineMeuse30min",
             # Raw url to config file
             config_url="https://raw.githubusercontent.com/UU-Hydro/PCR-GLOBWB_input_example/master/ini_and_batch_files_for_pcrglobwb_course/rhine_meuse_30min_using_input_example/setup_natural_test.ini",
--- tests/parameter_sets/__init__.py
+++ tests/parameter_sets/__init__.py
@@ -6,8 +6,8 @@ def test_download_example_parameter_sets(tmp_path):
     CFG["parameterset_dir"] = tmp_path / "parameters"
     download_example_parameter_sets()
 
-    assert (tmp_path / "parameters" / "pcrglobwb_example_case").exists()
-    assert (tmp_path / "parameters/pcrglobwb_example_case/setup_natural_test.ini").exists()
+    assert (tmp_path / "parameters" / "pcrglobwb_rhinemeuse_30min").exists()
+    assert (tmp_path / "parameters/pcrglobwb_rhinemeuse_30min/setup_natural_test.ini").exists()
 
 
 # TODO test for the case where ewatercycle.yaml cfg is not writable
