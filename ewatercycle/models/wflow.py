141673cda1054d896b972574e8563d9eec45dbfa
Peter Kalverla <peter.kalverla@gmx.com>
2021-07-16 11:48:15 +0200

    Switch back to using SIF image for wflow

--- ewatercycle/models/wflow.py
+++ ewatercycle/models/wflow.py
@@ -52,6 +52,13 @@ class Wflow(AbstractModel[WflowForcing]):
         }
         self.docker_image = images[self.version]
 
+    def _singularity_image(self, singularity_dir):
+        images = {
+            "2020.1.1": "ewatercycle-wflow-grpc4bmi_2020.1.1.sif",
+        }
+        image = singularity_dir / images[self.version]
+        return str(image)
+
     def _setup_default_config(self):
         config_file = self.parameter_set.config
         forcing = self.forcing
@@ -147,20 +154,11 @@ class Wflow(AbstractModel[WflowForcing]):
                 timeout=10,
             )
         elif CFG["container_engine"] == "singularity":
-            try:
-                self.bmi = BmiClientSingularity(
-                    image=f"docker://{self.docker_image}",
-                    work_dir=str(self.work_dir),
-                    timeout=15,
-                )
-            except FutureTimeoutError:
-                raise ValueError(
-                    "Couldn't spawn the singularity container within allocated"
-                    " time limit (15 seconds). You may try building it with "
-                    f"`!singularity run docker://{self.docker_image} run-bmi-server -h` and try "
-                    "again. Please also inform the system administrator that "
-                    "the singularity image was missing."
-                )
+            self.bmi = BmiClientSingularity(
+                image=self._singularity_image(CFG['singularity_dir']),
+                work_dir=str(self.work_dir),
+                timeout=15,
+            )
         else:
             raise ValueError(f"Unknown container technology: {CFG['container_engine']}")
 
