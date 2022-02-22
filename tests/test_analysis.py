5f1d83015732746bd637534de2b0cf80cddbc3c0
Peter Kalverla <peter.kalverla@gmx.com>
2022-02-22 16:25:41 +0100

    Update test and baseline image

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -7,12 +7,12 @@ from ewatercycle.analysis import hydrograph
 
 @image_comparison(
     baseline_images=["hydrograph"],
-    # remove_text=True,
+    remove_text=True,
     extensions=["png"],
     savefig_kwarg={"bbox_inches": "tight"},
 )
 def test_hydrograph():
-    ntime = 300
+    ntime = 3000
 
     dti = pd.date_range("2018-01-01", periods=ntime, freq="d")
 
