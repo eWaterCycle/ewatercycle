1d49732d2081efecab6b367f5261ad1cbdd86ddb
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-04-07 09:50:43 +0200

    Mock recipe.run with session argument

--- tests/forcing/test_lisflood.py
+++ tests/forcing/test_lisflood.py
@@ -50,10 +50,11 @@ def mock_recipe_run(monkeypatch, tmp_path):
             create_netcdf("e", tmp_path / "lisflood_e.nc"),
         )
 
-    def mock_run(self):
+    def mock_run(self, session=None):
         """Store recipe for inspection and return dummy output."""
         nonlocal data
         data["data_during_run"] = self.data
+        data["session"] = session
         return {"diagnostic_daily/script": MockTaskOutput()}
 
     monkeypatch.setattr(Recipe, "run", mock_run)
