683fa983cd9e4ddda3ab235d9309bb9222a1dc25
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2023-06-07 16:36:18 +0200

    Removed skipped tests from parameter_sets/test_example.py

--- tests/src/parameter_sets/test_example.py
+++ tests/src/parameter_sets/test_example.py
@@ -38,65 +38,6 @@ def example(setup_config, tmp_path: Path):
     return ps
 
 
-@pytest.mark.skip("ParameterSet.to_config is deprecated?")
-def test_to_config(example, tmp_path):
-    example.to_config()
-
-    assert "firstexample" in CFG.parameter_sets
-    expected = dict(
-        doi="N/A",
-        target_model="generic",
-        directory="mymodelexample",
-        config="mymodelexample/config.ini",
-        supported_model_versions={"0.4.2"},
-    )
-    assert CFG.parameter_sets["firstexample"] == expected
-
-
-@patch("urllib.request.urlopen")
-@patch("subprocess.check_call")
-@pytest.mark.skip("This downloader is removed. A replacement test has to be written.")
-def test_download(
-    mock_check_call, mock_urlopen, example, tmp_path  #: "ExampleParameterSet",
-):
-    example.config.unlink()
-    example.directory.rmdir()
-    ps_dir = tmp_path / "mymodelexample"
-    r = Mock()
-    r.read.return_value = b"somecontent"
-    mock_urlopen.return_value = r
-    mock_check_call.side_effect = lambda _: ps_dir.mkdir()
-
-    example.download(download_dir=ps_dir)
-
-    mock_urlopen.assert_called_once_with(
-        "https://github.com/mymodelorg/mymodelrepo/raw/master/mymodelexample/config.ini"
-    )
-    mock_check_call.assert_called_once_with(
-        [
-            "svn",
-            "export",
-            "https://github.com/mymodelorg/mymodelrepo/trunk/mymodelexample",
-            ps_dir,
-        ]
-    )
-    assert (ps_dir / "config.ini").read_text() == "somecontent"
-
-
-# TODO: Check with Stefan/Peter
-# should this check of the download dir exists, or the config path?
-# Also: ParameterSet.download only logs, does not raise error.
-@pytest.mark.skip("Superceded by the next test?")
-def test_download_already_exists(example, tmp_path: Path):
-    ps_dir = tmp_path / "mymodelexample"
-    ps_dir.mkdir(exist_ok=True)
-
-    with pytest.raises(ValueError) as excinfo:
-        example.download(download_dir=ps_dir)
-
-    assert "already exists, will not overwrite." in str(excinfo.value)
-
-
 def test_download_already_exists_but_skipped(example, tmp_path: Path, caplog):
     ps_dir = tmp_path / "mymodelexample"
     ps_dir.mkdir(exist_ok=True)
