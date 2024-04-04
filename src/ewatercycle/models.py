e42b3398b8f0fa347b4e9c3bee52b31ea78a5455
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2024-04-04 10:48:03 +0200

    Catch error upon import of models more gracefully (#404)
    * Catch error upon import of models more gracefully
    
    * Update changelog
    
    * Fix package name in pip uninstall suggestion
    
    * docs formatting
    
    * Update CHANGELOG.md
    
    Co-authored-by: Peter Kalverla <peter.kalverla@gmx.com>
    
    ---------
    
    Co-authored-by: Peter Kalverla <peter.kalverla@gmx.com>

--- src/ewatercycle/models.py
+++ src/ewatercycle/models.py
@@ -20,7 +20,7 @@ registered in the :py:data:`ewatercycle.models`
 `entry point group <https://packaging.python.org/en/latest/specifications/entry-points/>`_.
 """
 
-from importlib.metadata import entry_points
+from importlib.metadata import entry_points, packages_distributions
 from typing import Type
 
 from ewatercycle import shared
@@ -28,9 +28,31 @@ from ewatercycle.base.model import eWaterCycleModel
 
 _model_entrypoints = entry_points(group="ewatercycle.models")  # /NOSONAR
 
+
+def get_package_name(module_name: str) -> str:
+    """Get the (pip) package name from a module name.
+
+    Note: falls back to [unknown] if the lookup fails.
+    """
+    if module_name in packages_distributions():
+        return packages_distributions()[module_name][0]
+    return "[unknown]"
+
+
 # Expose as "from ewatercycle.models import Model" for backward compatibility
 for _model in _model_entrypoints:
-    globals()[_model.name] = _model.load()
+    try:
+        globals()[_model.name] = _model.load()
+    except Exception as e:
+        msg = (
+            "An error was raised when trying to load the plugin of model "
+            f"'{_model.name}'.\n"
+            "You can report the issue on the model's github repository, "
+            "or on https://github.com/eWaterCycle/ewatercycle/issues\n"
+            "In the meantime, you can try uninstalling the plugin with:\n"
+            f"    pip uninstall {get_package_name(_model.value.split('.')[0])}"
+        )
+        raise ImportError(msg) from e
 
 
 class ModelSources(shared.Sources):
