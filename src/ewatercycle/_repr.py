da7bc48369cf3a5326337a6b1af885d989a87bd6
Mark Melotto <70904313+MarkMelotto@users.noreply.github.com>
2025-03-04 12:17:30 +0100

    Update _repr.py
    ruff appropriate in my VScode

--- src/ewatercycle/_repr.py
+++ src/ewatercycle/_repr.py
@@ -66,7 +66,7 @@ class Representation:
 
     def __repr__(self) -> str:
         """The default implementation of __repr__ just calls __str__."""
-        return f'{self.__repr_name__()}({self.__repr_str__(", ")})'
+        return f"{self.__repr_name__()}({self.__repr_str__(', ')})"
 
     def __rich_repr__(self) -> RichReprResult:
         """Get fields for Rich library."""
