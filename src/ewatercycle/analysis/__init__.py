82afccac78b8c095c98ba89b6a030cb7269e99e9
andrevdv <220902110+andrevdv@users.noreply.github.com>
2025-12-10 09:49:03 +0100

    init py

--- src/ewatercycle/analysis/__init__.py
+++ src/ewatercycle/analysis/__init__.py
@@ -3,9 +3,3 @@ from .hydrograph import hydrograph
 
 __all__ = ["hydrograph"]
 
-#TODO add more analysis functions?
-# - yearly stats
-# - cumulative stats
-# - various hydrological stats, see written down notes
-# - long term stats - climate change analysis
-# - extreme value analysis
