140c70be519fa3bdb303ed3ebbd089fe5adffc3c
Mark Melotto <markmelotto@tudelft.nl>
2026-02-19 10:32:12 +0100

    lint checks

--- src/ewatercycle/_forcings/caravan.py
+++ src/ewatercycle/_forcings/caravan.py
@@ -114,11 +114,11 @@ class CaravanForcing(DefaultForcing):
         Set this variable to the directory containing the netCDF files.
         On an eWaterCycle machine this path is: /data/shared/climate-data/caravan.
         On other machines it depends on where the data is located.
-        If you have created a locally available copy of the data at /path/to/caravan_data,
+        If you have created a locally available copy of the data: /path/to/caravan_data,
         use the following command in your terminal;
             export CARAVAN_CACHE=/path/to/caravan_data
         To always have this environment variable set, add this to .bashrc or
-        ask your system administrator. 
+        ask your system administrator.
 
         Args:
             dataset (str): name of dataset, choose from:
